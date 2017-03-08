fde9a277315defe17d905b79a598d941c823b050
Christopher Guthrie <christopher.guthrie@blackrock.com>
2017-03-08 16:15:04 +0000

    Adding other misc files

---
+++ test.js
@@ -0,0 +1,48 @@
+// Load the SDK
+const AWS = require('aws-sdk');
+const Stream = require('stream');
+const Speaker = require('speaker');
+require('/home/pi/Downloads/polly_poc/awsauth.js');
+
+// Create an Polly client
+const Polly = new AWS.Polly({
+    signatureVersion: 'v4',
+    region: 'us-east-1'
+});
+
+// Create the Speaker instance
+let Player = new Speaker({
+    channels: 1,
+    bitDepth: 16,
+    sampleRate: 8000
+})
+
+var AudioStream = new Stream.Readable();
+AudioStream._read = function () {};
+AudioStream.pipe(Player);
+
+/**
+ * Use Amazon Polly
+ * @param  {string} text    Text to speek
+ * @param  {string} VoiceId (optional) Amazon Polly Voice
+ */
+let speak = function(text){
+    let params = {
+        'Text': text,
+        'SampleRate': "8000",
+        'OutputFormat': 'pcm',
+        'VoiceId': 'Justin'
+    };
+
+    Polly.synthesizeSpeech(params, (err, data) => {
+        if (err) {
+            console.log(err.code)
+        } else if (data) {
+            if (data.AudioStream instanceof Buffer) {
+                AudioStream.push(data.AudioStream)
+            }
+        }
+    })
+};
+
+speak("Aladdin Risk for Wealth Management is bloody brilliant!. ");
