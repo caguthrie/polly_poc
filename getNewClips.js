3792fc3c8d0e2102bc60d7dc72dc6bd981f86f50
Christopher Guthrie <christopher.guthrie@blackrock.com>
2017-03-09 23:36:56 -0500

    Reading title of audio clip

--- getNewClips.js
+++ getNewClips.js
@@ -9,11 +9,6 @@ require('getmac').getMac(function(err,macAddress){
     fetch(`http://rockstore.herokuapp.com/is_there_new_clip/${macAddress}`)
         .then(res => res.json())
         .then(json => {
-            if( json.length > 0 ){
-				//let cmd = "sudo python /home/pi/Downloads/rpi_ws281x/python/examples/glow_pulse.py pulse"
-				//const process = child_process.exec(cmd);
-				//fs.appendFile('/home/pi/pids', process.pid + "\n", function (err) {});
-            }
             fs.writeFile("./newClips.json", JSON.stringify(json), function(err) {
                 if(err) {
                     return console.log(err);
--- playClipFromWeb.js
+++ playClipFromWeb.js
@@ -3,6 +3,68 @@ const request = require('request');
 const Player = require('player');
 const _ = require('lodash');
 const getmac = require('getmac');
+const Q = require('q');
+
+// Load the SDK
+const AWS = require('aws-sdk');
+const Stream = require('stream');
+const Speaker = require('speaker');
+require('/home/pi/Downloads/polly_poc/awsauth.js');
+
+/**
+ * Start Polly setup
+ */
+
+
+// Create an Polly client
+const Polly = new AWS.Polly({
+  signatureVersion: 'v4',
+  region: 'us-east-1'
+});
+
+// Create the Speaker instance
+let Player = new Speaker({
+  channels: 1,
+  bitDepth: 16,
+  sampleRate: 8000
+});
+
+let AudioStream = new Stream.Readable();
+AudioStream._read = () => {return {}};
+AudioStream.pipe(Player);
+
+let deferred = [];
+
+// Gross hack for hackathon: After line 274 in node_modules/speaker/index.js
+// self.doneWithChunk && self.doneWithChunk();
+Player.doneWithChunk = () => {
+  setTimeout(() => {
+    const lastDeferred = _.find(deferred, (def) => def.promise.inspect().state === "pending");
+    lastDeferred.resolve();
+  }, 2000);
+};
+
+let speak = (text) => {
+  deferred = deferred.filter((def) => def.promise.inspect().state === "pending");
+  deferred.push(Q.defer());
+  let params = {
+    'Text': text,
+    'SampleRate': "8000",
+    'OutputFormat': 'pcm',
+    'VoiceId': 'Brian'
+  };
+
+  Polly.synthesizeSpeech(params, (err, data) => {
+    if (err) {
+      console.log(err.code)
+    } else if (data) {
+      if (data.AudioStream instanceof Buffer) {
+        AudioStream.push(data.AudioStream)
+      }
+    }
+  });
+  return Q.all(deferred.map((def) => def.promise));
+};
 
 const SERVER_ROOT = "http://";
 
@@ -48,20 +110,22 @@ fs.readFile('./newClips.json', 'utf8', function (err, data) {
         process.exit(0);
     }
     else{
-        require('getmac').getMac(function(err,macAddress) {
-            // TODO this is a hack -- only play the first clip found
-            download(SERVER_ROOT + clips[0].clip.substring(2,99999), 'out.mp3', () => {
-                const player = new Player('./out.mp3');
-                request.get(`http://rockstore.herokuapp.com/history/${clips[0].id}/${macAddress}`);
-                // play now and callback when playend
-                player.play(function (err, player) {
-                    console.log('playend!');
-                }).on('error', function (err) {
-                    // Dont care about this
-                });
-
+      speak(`Reading clip entitled: ${clips[0].title}`).then(function() {
+        require('getmac').getMac(function (err, macAddress) {
+          // TODO this is a hack -- only play the first clip found
+          download(SERVER_ROOT + clips[0].clip.substring(2, 99999), 'out.mp3', () => {
+            const player = new Player('./out.mp3');
+            request.get(`http://rockstore.herokuapp.com/history/${clips[0].id}/${macAddress}`);
+            // play now and callback when playend
+            player.play(function (err, player) {
+              console.log('playend!');
+            }).on('error', function (err) {
+              // Dont care about this
             });
+
+          });
         });
+      });
     }
 });
 
