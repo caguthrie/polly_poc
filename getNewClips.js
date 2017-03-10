6d39f329a8d19553a00c07d3ce2a98d7c933d0da
Christopher Guthrie <christopher.guthrie@blackrock.com>
2017-03-10 09:55:23 -0500

    curl command hack

--- getNewClips.js
+++ getNewClips.js
@@ -1,20 +1,18 @@
 const fs = require('fs');
-const fetch = require('node-fetch');
+const http = require('http');
 const getmac = require('getmac');
 const child_process = require('child_process');
 
 require('getmac').getMac(function(err,macAddress){
     if (err)  throw err;
     console.log(macAddress);
-    fetch(`http://rockstore.herokuapp.com/is_there_new_clip/${macAddress}`)
-        .then(res => res.json())
-        .then(json => {
-            fs.writeFile("./newClips.json", JSON.stringify(json), function(err) {
-                if(err) {
-                    return console.log(err);
-                }
-                console.log("The file was saved!");
-                process.exit(0);
-            });
-        });
+    const result = child_process.execSync(`curl http://rockstore.herokuapp.com/is_there_new_clip/${macAddress}`);
+    console.log(result.toString());
+    fs.writeFile("./newClips.json", result.toString(), function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+        process.exit(0);
+    });
 });
